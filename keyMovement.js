b00b423998f928149b25fe82dcb117f3abf1b75b
William Robertson <w.robertson305@gmail.com>
2018-12-09 04:43:03 +0000

    Improvements to inertia (movement, not look)

--- keyMovement.js
+++ keyMovement.js
@@ -13,7 +13,7 @@ var KeyMovement = function (camera, matrixOps, rotation, mouseMovement) {
   this.residualOY = 0
 
   this.residualMX = 0
-  this.residualMY = 10 // set an initial move
+  this.residualMY = 2.5 // set an initial move
   this.residualMZ = 0
   this.lookScale = 1
   this.moveScale = 2
@@ -66,37 +66,63 @@ KeyMovement.prototype = {
 
     var orientMatrix = this.rotation.getXYZRotMat(orientX, orientY, orientZ)
 
+    var topSpeed = 50
+
     // keys control camera position
     var moveMatrix = this.matrixOps.getIdentity4()
     // forward (w s)
     if (activeKeys[87]) {
-      moveMatrix[2][3] = -10 * this.moveScale
+      moveMatrix[2][3] = -3 * this.moveScale
     } else if (activeKeys[83]) {
-      moveMatrix[2][3] = 10 * this.moveScale
+      moveMatrix[2][3] = 3 * this.moveScale
     } else {
-      moveMatrix[2][3] = this.residualMZ
+      moveMatrix[2][3] = 0
     }
-    this.residualMZ = moveMatrix[2][3] * 0.995
+    moveMatrix[2][3] += this.residualMZ
+
+    if(moveMatrix[2][3] > 0) {
+      moveMatrix[2][3] = Math.min( moveMatrix[2][3], topSpeed)
+    } else {
+      moveMatrix[2][3] = Math.max( moveMatrix[2][3], -topSpeed)
+    }
+
+    this.residualMZ = moveMatrix[2][3] * 0.9995
 
     // sideways (a d)
     if (activeKeys[65]) {
-      moveMatrix[0][3] = -5 * this.moveScale
+      moveMatrix[0][3] = -2 * this.moveScale
     } else if (activeKeys[68]) {
-      moveMatrix[0][3] = 5 * this.moveScale
+      moveMatrix[0][3] = 2 * this.moveScale
     } else {
-      moveMatrix[0][3] = this.residualMX
+      moveMatrix[0][3] = 0
     }
-    this.residualMX = moveMatrix[0][3] * 0.995
+    moveMatrix[0][3] += this.residualMX
+
+    if(moveMatrix[0][3] > 0) {
+      moveMatrix[0][3] = Math.min( moveMatrix[0][3], topSpeed)
+    } else {
+      moveMatrix[0][3] = Math.max( moveMatrix[0][3], -topSpeed)
+    }
+
+    this.residualMX = moveMatrix[0][3] * 0.9995
 
     // up/down (x c)
     if (activeKeys[88]) {
-      moveMatrix[1][3] = 5 * this.moveScale
+      moveMatrix[1][3] = 2 * this.moveScale
     } else if (activeKeys[67]) {
-      moveMatrix[1][3] = -5 * this.moveScale
+      moveMatrix[1][3] = -2 * this.moveScale
     } else {
-      moveMatrix[1][3] = this.residualMY
+      moveMatrix[1][3] = 0
     }
-    this.residualMY = moveMatrix[1][3] * 0.995
+    moveMatrix[1][3] += this.residualMY
+
+    if(moveMatrix[1][3] > 0) {
+      moveMatrix[1][3] = Math.min( moveMatrix[1][3], topSpeed)
+    } else {
+      moveMatrix[1][3] = Math.max( moveMatrix[1][3], -topSpeed)
+    }
+
+    this.residualMY = moveMatrix[1][3] * 0.9995
 
     var positionMatrix = this.matrixOps.matMul(moveMatrix, orientMatrix)
     this.camera.move(positionMatrix)
