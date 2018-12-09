93e62eb0fc4b51d62906ed92b3dd28aa17c5746a
William Robertson <w.robertson305@gmail.com>
2018-12-09 14:01:56 +0000

    Added some comments to projector.js

--- frontEnd/projector.js
+++ frontEnd/projector.js
@@ -1,5 +1,6 @@
 /**
 * Projects an array of 3d vectors onto a plane, returning an array of 2d vectors.
+* Basically I just read this: https://en.wikipedia.org/wiki/3D_projection
 */
 var Projector = function Projector () {
 }
@@ -13,16 +14,16 @@ Projector.prototype = {
 // Az = distance of point A from viewer (in the z axis)
 
   mapPointsArrToPlane: function mapPointsArrToPlane (arr) {
-  // 'my location ' after the transform to a coord system with the camera at 0 and not rotated
+    
+    // ml === 'my location', after the transform to a coord system with the camera at 0 and not rotated
     var ml = [0, 0, 2]
     var mapped = []
     for (var i = 0; i < arr.length; i++) {
       var point = arr[i]
-      if (point.z < 0) {
-      // console.log("My distance from object: " + Az)
+      if (point.z < 0) { // if it's behind us, we don't want to draw it! (todo: frustrum clipping)
+        // Az: distance of the point to be mapped from the viewer
         var Az = ml[2] - point.z
-        // console.log("My distance from camera: " + Bz)
-        // var Bz = ml[2] - camLocation[2]
+        // Bz: distance of the screen from the viewer (since the camera is a 0, it's just the z coordinate of ml)
         var Bz = ml[2]
 
         // x
@@ -34,8 +35,6 @@ Projector.prototype = {
         var By = Ay * (Bz / Az)
 
         mapped.push([Bx, By])
-      } else {
-        // mapped[i] = 'NORENDER'
       }
     }
     return mapped
